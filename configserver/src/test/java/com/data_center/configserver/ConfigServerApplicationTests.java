f0bf301fcb9f7d89158919c819dd9e8bb4c6d097
Кирилл Морозов <theeighthofseptember@gmail.com>
2025-12-03 19:49:57 +0300

    add configserver

---
+++ configserver/src/test/java/com/data_center/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.data_center.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
