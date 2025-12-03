c58a3e302ebebc5d031e9476e84e8b6e25610b35
Кирилл Морозов <theeighthofseptember@gmail.com>
2025-12-03 14:01:47 +0300

    public class Servers update

--- src/main/java/com/data_center/servers/service/ServersService.java
+++ src/main/java/com/data_center/servers/service/ServersService.java
@@ -19,18 +19,41 @@ public class ServersService {
 
     // GET - получение данных о сервере
     public Servers getServer(String datacenterName, String serverName) {
+
         Servers server = new Servers();
+
         server.setId(new Random().nextInt(1000));
+
+        // Общая информация
         server.setDatacenterName(datacenterName);
         server.setServerName(serverName);
-        server.setActiveUse(true);
-        server.setPowerConsumption(450);
-        server.setCost(150000);
-        server.setSerialNumber("SN-" + new Random().nextInt(99999));
-        server.setCpu("Intel Xeon Gold 6248R");
-        server.setRam("128GB DDR4");
-        server.setStorage("2TB NVMe SSD");
-        server.setOs("Ubuntu Server 22.04");
+
+        // Аппаратная конфигурация
+        server.setCpuModel("Intel Xeon Gold 6248R");
+        server.setCpuCores(24);
+        server.setRamGB(128);
+        server.setStorageType("NVMe SSD");
+        server.setStorageGB(2000);
+
+        // Энергопотребление
+        server.setIdlePowerW(180.0);
+        server.setMaxPowerW(450.0);
+        server.setPsuEfficiency(0.92);
+
+        // Характеристики ЦОД
+        server.setCoolingType("air");
+        server.setCoolingSystemType("direct free cooling");
+        server.setAirflowLayout("cold/hot aisle containment");
+        server.setEnvironmentRegion("Умеренный климат");
+        server.setRackDensityKW(8.0);
+        server.setNetworkTopology("spine-leaf");
+        server.setPue(1.20);
+
+        // Программная среда
+        server.setOperatingSystem("Ubuntu Server 22.04");
+        server.setVirtualizationType("container");
+        server.setWorkloadType("web services");
+
         return server;
     }
 
@@ -57,10 +80,10 @@ public class ServersService {
     }
 
     // DELETE - удаление сервера
-    public String deleteServer(String datacenterName, String serverName, int powerConsumption,  Locale locale) {
+    public String deleteServer(String datacenterName, String serverName, int maxPowerW,  Locale locale) {
         return String.format(messages.getMessage("servers.delete.message", null, locale),
                 serverName,
-                powerConsumption,
+                maxPowerW,
                 datacenterName);
     }
 
